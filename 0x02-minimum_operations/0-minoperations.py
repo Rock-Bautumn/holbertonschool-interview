1c0e51c21797c901db9dfa6a4e8a84afd73c7a91
Rock-Bautumn <3623@holbertonschool.com>
2023-02-28 18:01:52 -0600

    Adding minimum operations project

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/bin/bash python3
+"""This script finds the lowest number of operations (see readme)"""
+
+def minOperations(n):
+    """Find the lowest number of operations"""
+
+    factor = 2
+    fac_sum = 0
+    result = 0
+
+    if n < 2:
+        return 0
+
+    while (n % factor > 0):
+        factor += 1
+
+    while (n % factor == 0):
+        result += 1
+        fac_sum += factor - 1
+        n /= factor
+        if (n == 1):
+            return result + fac_sum
+        while (n % factor > 0):
+            factor += 1
+
+    return result
